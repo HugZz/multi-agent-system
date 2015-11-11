8245cee3c460c695619dd8bf7e0c506a864b739d
Hugues de Valon <hugues.devalon@gmail.com>
2015-11-11 16:07:47 +0100

    Utilisation des évènements pour les boids

--- src/boids/BoidsSimulator.java
+++ src/boids/BoidsSimulator.java
@@ -5,10 +5,13 @@ public class BoidsSimulator implements gui.Simulable {
     private Boids boids;
     private GUISimulator guiBoids;
     private static final int r=10 ;
+    private EventManager manager;
 
     public BoidsSimulator(int n, GUISimulator g){
         this.setBoids(new Boids(n));
         this.setGuiBoids(g);
+        this.manager = new EventManager();
+        this.manager.addEvent(new BoidsEvent(this.boids, 1, manager));
     }
 
     public Boids getBoids(){
@@ -27,13 +30,16 @@ public class BoidsSimulator implements gui.Simulable {
 
     @Override
     public void next(){
-        this.getBoids().step();
+        //this.getBoids().step();
+        this.manager.next();
         this.getGuiBoids().reset();
         this.affiche();
     }
     @Override
     public void restart(){
+        this.manager.restart();
         this.getBoids().reInit();
+        this.manager.addEvent(new BoidsEvent(this.boids, 1, this.manager));
         this.getGuiBoids().reset();
         this.affiche();
     }
