ee748434b9761d57bea6d7ef4647ad476170958c
Hugues de Valon <hugues.devalon@gmail.com>
2015-11-14 17:35:38 +0100

    réglage pour plusieurs groupes

--- src/final_src/BBEvent.java
+++ src/final_src/BBEvent.java
@@ -1,15 +1,17 @@
 public class BBEvent extends Event {
-    private Balls balls;
-    private int delai;
+    private Balls[] balls;
+    private int[] delai;
 
-    public BBEvent(Balls balls, long date, EventManager manager, int delai) {
+    public BBEvent(Balls[] balls, long date, EventManager manager, int[] delai) {
         super(date, manager);
         this.balls = balls;
         this.delai = delai;
     }
 
     public void execute() {
-        this.balls.actualiser();
-        super.getManager().addEvent(new BBEvent(this.balls, super.getDate() + this.delai, this.getManager(), this.delai));
+        for(int i = 0; i < this.balls.length; i++) {
+            this.balls[i].actualiser();
+            super.getManager().addEvent(new BBEvent(this.balls, super.getDate() + this.delai[i], this.getManager(), this.delai));
+        }
     }
 }
--- src/final_src/BBSimulator.java
+++ src/final_src/BBSimulator.java
@@ -1,30 +1,34 @@
 import java.awt.Color;
 import gui.*;
 public class BBSimulator extends Simulator {
-    private Balls balls;
+    private Balls[] balls;
     private static int r = 10;
 
-    public Balls getBalls() {
+    public Balls[] getBalls() {
         return this.balls;
     }
 
-    public void setBalls(Balls b) {
+    public void setBalls(Balls[] b) {
         this.balls = b;
     }
 
-    public BBSimulator(GUISimulator simulator, Balls balles, Event event, EventManager manager) {
+    public BBSimulator(GUISimulator simulator, Balls[] balles, Event event, EventManager manager) {
         super(simulator, event, manager);
         this.setBalls(balles);
     }
 
     public void affiche() {
-        for(int i=0; i < this.getBalls().getNbBalls(); i++) {
-            super.getGUI().addGraphicalElement(new Oval(this.getBalls().getPx(i),this.getBalls().getPy(i),Color.BLUE,Color.RED,r));
+        for(int j = 0; j < this.balls.length; j++) {
+            for(int i=0; i < this.getBalls()[j].getNbBalls(); i++) {
+                super.getGUI().addGraphicalElement(new Oval(this.getBalls()[j].getPx(i),this.getBalls()[j].getPy(i),Color.BLUE,Color.RED,r));
+            }
         }
     }
 
     public void reInit()
     {
-        this.getBalls().reInit();
+        for(int i = 0; i < this.balls.length; i++) {
+            this.getBalls()[i].reInit();
+        }
     }
 }
